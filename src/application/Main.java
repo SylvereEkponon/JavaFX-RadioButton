a48f1948af685760885012137fa3c03447aa6444
Sylvere Ekponon <tekponon@my.centennialcollege.ca>
2017-11-13 19:11:05 -0500

    Updated Controller and Main.fxml

--- src/application/Main.java
+++ src/application/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("application/Main.fxml"));
-			Scene scene = new Scene(root,400,400);
+			Parent root = FXMLLoader.load(getClass().getResource("/application/Main.fxml"));
+			Scene scene = new Scene(root,300,300);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
--- src/application/MainController.java
+++ src/application/MainController.java
@@ -1,7 +1,41 @@
 package application;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
 
 public class MainController {
+	
+	@FXML
+	private RadioButton rbMale;
+		
+	@FXML
+	private RadioButton rbFemale;
+	
+	@FXML
+	private RadioButton rbNotToSay;
+	
+	@FXML
+	private Label lbl;
+	
+	public void SelectEvent(ActionEvent event) {
+		String output="";
+		
+		if (rbMale.isSelected()) {
+			output = rbMale.getText();
+		} 
+		
+		if (rbFemale.isSelected()) {
+			output = rbFemale.getText();
+		} 
+		
+		if(rbNotToSay.isSelected()) {
+			output = rbNotToSay.getText();
+		}
+		
+		lbl.setText("Your gender is " +output);
+		
+	}
 
 }
